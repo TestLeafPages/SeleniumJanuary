940f8706924cb4b81865756e9b5d931ef11509cd
TestLeafPages <sarath.muthuswamy@testleaf.com>
2020-01-26 09:32:34 +0530

    Add files via upload

---
+++ week1/day2/classroom/arrays/PrintDuplicatesInArray.java
@@ -0,0 +1,29 @@
+package day2;
+
+public class PrintDuplicatesInArray {
+
+	public static void main(String[] args) {
+		
+		int[] arr = {11,12,13,14,15,15,16,16,17,17,18,19,20};
+		
+		//	get the length of the array
+		
+		//	declare an int variable named count
+		
+		//	iterate from 0 to the array length-1 (outer loop starts here)
+		
+			//	assign 0 to count 
+			
+			//	iterate from i to the length of the array by adding 1 to it (inner loop starts here)
+			
+					//	compare both the loop variables & check they're equal
+				
+							//	increase the count if both the arrays are equal
+					
+			//	Out of the inner loop, check and print the first array variable if count is more than 0
+			
+			
+		
+	}
+
+}
