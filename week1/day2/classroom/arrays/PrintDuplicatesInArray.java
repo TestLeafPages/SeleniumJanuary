c04f0e07bc6b418f329a92069ebdba9e067acd2e
TestLeafPages <sarath.muthuswamy@testleaf.com>
2020-01-26 13:56:05 +0530

    Update PrintDuplicatesInArray.java

--- week1/day2/classroom/arrays/PrintDuplicatesInArray.java
+++ week1/day2/classroom/arrays/PrintDuplicatesInArray.java
@@ -4,7 +4,7 @@ public class PrintDuplicatesInArray {
 
 	public static void main(String[] args) {
 		
-		int[] arr = {11,12,13,14,15,15,16,16,17,17,18,19,20};
+		int[] arr = {14,12,13,11,15,14,18,16,17,19,18,17,20};
 		
 		//	get the length of the array
 		
