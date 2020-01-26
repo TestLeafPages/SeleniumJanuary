a2d17d3c20233f3dbfe16a0ab4966b7b24337161
TestLeafPages <sarath.muthuswamy@testleaf.com>
2020-01-26 13:53:15 +0530

    Update MissingElementInAnArray.java

--- week1/day2/classroom/arrays/MissingElementInAnArray.java
+++ week1/day2/classroom/arrays/MissingElementInAnArray.java
@@ -4,7 +4,7 @@ public class MissingElementInAnArray {
 
 	public static void main(String[] args) {
 		// Here is the input
-		int[] arr = {2,3,4,7,6,8};
+		int[] arr = {1,2,3,4,7,6,8};
 
 		// Sort the array	
 		
