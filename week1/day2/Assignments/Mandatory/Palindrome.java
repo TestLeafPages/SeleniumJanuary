bb48ed360dec8f021e6ebdd788f67b912f628ff3
TestLeafPages <sarath.muthuswamy@testleaf.com>
2020-01-26 14:11:58 +0530

    Add files via upload

---
+++ week1/day2/Assignments/Mandatory/Palindrome.java
@@ -0,0 +1,24 @@
+package week1.Assignments;
+
+public class Palindrome {
+	//Build a logic to find the given string is palinDrome or not
+     
+	/*
+
+	 * Pseudo Code
+	
+	 * a) Declare A String value as"madam"
+	 
+	 * b) Declare another String rev value as ""
+
+	 * c) Iterate over the String in reverse order
+
+	 * d) Add the char into rev
+
+	 * e) Compare text and rev, if it is same then print palinDrome 
+	 
+	 * Hint: Use .equals or .equalsIgnoreCase when you compare a String 
+
+	 */
+
+}
---
+++ week1/day2/Assignments/Optional/Anagram.java
@@ -0,0 +1,31 @@
+package week1.Assignments;
+
+public class Anagram {
+	
+	/*
+
+	 * Pseudo Code
+	  
+	 *Declare a String 
+
+				String text1 = "stops";
+
+	 *Declare another String
+
+				String text2 = "potss"; 
+
+	 * a) Check length of the strings are same then (Use A Condition)
+
+	 * b) Convert both Strings in to characters
+
+	 * c) Sort Both the arrays
+
+	 * d) Check both the arrays has same value
+
+	 * 
+
+	 */
+
+
+
+}
