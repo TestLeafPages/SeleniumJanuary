a153574614969c0585e0cc8c523bd0e108956d51
TestLeafPages <sarath.muthuswamy@testleaf.com>
2020-01-26 08:32:52 +0530

    Add files via upload

---
+++ day2/CharOccurance.java
@@ -0,0 +1,27 @@
+package day2;
+
+public class CharOccurance {
+	
+	public static void main(String[] args) {
+//		Check number of occurrences of a char in a String
+		
+		String str = "welcome to chennai";
+		int count = 0;
+//		convert the string into char array
+		
+//		get the length of the array
+		
+//		traverse from 0 till the array length 
+		
+//			Check the char array has the particular char in it (you can use the variable here)
+			
+//				if is has increment the count
+				
+			 
+		
+//		print the count out of the loop
+		
+		
+	}
+
+}
