9b992ce2aff9c9738f3e43deedc0a41b4bf94249
TestLeafPages <sarath.muthuswamy@testleaf.com>
2020-01-31 18:06:25 +0530

    Add files via upload

---
+++ week2/day1.assignments/FindAccount.java
@@ -0,0 +1,45 @@
+package week2.day1.Assignments;
+
+public class FindAccount {
+
+	public static void main(String[] args) {
+		/*
+		 * //Pseudo Code
+		 * 
+		 * 1. Launch URL "http://leaftaps.com/opentaps/control/login"
+		 * 
+		 * 2. Enter UserName and Password Using Id Locator
+		 * 
+		 * 3. Click on Login Button using Class Locator
+		 * 
+		 * 4. Click on CRM/SFA Link
+		 * 
+		 * 5. Click on Accounts Button
+		 * 
+		 * 6. Click on Find Accounts
+		 * 
+		 * 7. Enter AccountName as "Credit Limited Account" 
+		 *  
+		 * 8. Click on Find Accounts using xpath Locator
+		 * 
+		 * 9. Click on the  Lead
+		 * 
+		 * 10. Get the Text of Account Name and verify 
+		 *  
+		 * 11. Get the Text of Description
+		 * 
+		 * 12. Click on the widget of New Team Member
+		 *  
+		 * 13. Click on the first Resulting team member Under Name
+		 *  
+		 * 14. Click on the Add button using 
+		 * 
+		 * 15. Verify whether Team Members are Assigned to this account under
+		  Team Members are Assigned to this account field.
+		 *  
+		 * 16. Get the title of the page and verify it.
+		 */
+
+	}
+
+}
---
+++ week2/day2.classroom/Myntra.java
@@ -0,0 +1,39 @@
+package seleniumCR;
+
+public class Myntra {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * //Pseudo Code
+		 * 
+		 * 1. Launch URL "https://www.flipkart.com/"
+		 * 
+		 * 2. Maximize the browser
+		 * 
+		 * 3. Search 'Shirt' in the Search bar [find it using Xpath]
+		 * 
+		 * 4. Check U.S Polo Assn under brand
+		 * 
+		 * 5. Check Customer Ratings 4★ & above
+		 * 
+		 * 6. Get all the names of shirts available and store it in a List
+		 * 
+		 * 7. Print all the names of the Shirts
+		 * 
+		 * 8. Click the first Resulting Shirt
+		 * 
+		 * 9. Select your Respective Size
+		 * 
+		 * 10. Click on Add to cart
+		 * 
+		 * 11 Verify whether the Product is added to Cart
+		 * 
+		 * 12. Get the title of the Resulting Page
+		 *  
+		 */
+		
+		
+	}
+
+}
