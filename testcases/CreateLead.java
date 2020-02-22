212c26d1bacb96958dd6a5eebbf636d25b45b66b
TestLeafPages <sarath.muthuswamy@testleaf.com>
2020-02-22 10:09:55 +0530

    TestNG Code

---
+++ testcases/CreateLead.java
@@ -0,0 +1,36 @@
+package testcases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class CreateLead {
+	@Test
+	public void createNewLead() {
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.findElementById("username").sendKeys("DemoSalesManager");
+		driver.findElementById("password").sendKeys("crmsfa");
+		driver.findElementByClassName("decorativeSubmit").click();
+		driver.findElementByLinkText("CRM/SFA").click();
+		driver.findElementByLinkText("Leads").click();
+		driver.findElementByLinkText("Create Lead").click();
+		driver.findElementById("createLeadForm_companyName").sendKeys("TestLeaf");
+		driver.findElementById("createLeadForm_firstName").sendKeys("Gopi");
+		driver.findElementById("createLeadForm_lastName").sendKeys("J");
+		driver.findElementByName("primaryPhoneNumber").sendKeys("9994229611564");
+		driver.findElementByName("submitButton").click();
+		driver.close();
+		
+	}
+}
+
+
+
+
+
+
